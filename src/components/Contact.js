c407bb2cd3a4535253a9d3bb130216104626868f
dylan-gregory <jdylangregory@gmail.com>
2017-08-29 20:57:29 -0400

    Added copy for about the program and about the founder

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,12 @@ class Contact extends Component {
   render(){
     return (
       <div className="container">
-        Contact
+        <div>
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;My name is Bridget Vogel Gregory and I am a certified health coach, turned vision coach. I also have a B.A. in Communication Studies. I dove into coaching because I believe that the right accountability, support and stretch are truly what help us change ourselves for the better. However, as much as I loved talking with clients about their health and setting them on a better path in that area of life, something was missing in my own.
+          Speaking with clients, and recalling years of mentoring young girls in volatile life seasons, caused me to realize that I seemed to thrive when it came to talking about life goals and vision. And so Ziel Coaching was birthed. I am so excited to go on a journey with you to discover your best life because I know you can do whatever is in your heart and mind to do. I truly believe that.
+          When I’m not coaching clients, reading books, or writing articles, I’m spending time with my husband Dylan. We are expecting our first child in January 2018 which I’m sure will teach me so much more. I’m a musician and songwriter in that spare time, and enjoy educating myself in natural wellness, namely essential oil uses. My husband and I currently reside in Greenville, South Carolina.
+
+        </div>
       </div>
     );
   }
