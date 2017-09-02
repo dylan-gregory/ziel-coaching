f48f476591385fa4135aa1568ec3b95838a5dbee
dylan-gregory <jdylangregory@gmail.com>
2017-09-02 17:35:35 -0400

    Making use of columned text for longer excerpts, sprucing up About page

--- src/components/About.js
+++ src/components/About.js
@@ -14,29 +14,34 @@ class About extends Component {
 
 
           <div className="about-p">
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Maybe because of outside circumstances, or maybe because of your own sense of doubt it couldn't be achieved. Maybe you're an entrepreneur with incredible business ideas, but something is holding you back from turning those ideas into reality. Maybe you're simply stuck, whether creatively, in your career, or in your general life, and you want to discover what really motivates you to thrive.
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Maybe because of outside circumstances, or maybe because of your own sense of doubt, it couldn't be achieved. Maybe you're an entrepreneur with incredible business ideas, but something is holding you back from turning those ideas into reality. Maybe you're simply stuck, whether creatively, in your career, or in your general life, and you want to discover what really motivates you to thrive.
 
-            <div className="that-ziel">
-              the things that make us tick,
-              that spur us toward adventure
-              ...THAT is Ziel
-            </div>
+
           <div>
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;If you identify with any of these things, I've been in your shoes. Apart from very kind people giving me a pat on the back and saying "you can do it," I've personally been hard pressed to find the strength within myself to see a vision for my life and then make it happen. I believe too many of us today aren't living our best, most fulfilling life. I don't want that for myself and I certainly don't want that for you.
-            This is why I developed Ziel Coaching. Our heart and soul, the things that make us tick, that spur us toward adventure, that get us excited when we talk about them, that is Ziel. My vision is to come alongside you as you discover those deep rooted desires, coach you through what may be stopping or slowing you down from achieving them, and cheer you on into a bright, productive and expressive future.
+
+            <div className="that-ziel">
+              "the things that make us tick, that spur us toward adventure...that is Ziel"
+            </div>
+
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;This is why I developed Ziel Coaching. Our heart and soul, the things that make us tick, that spur us toward adventure, that get us excited when we talk about them, that is Ziel. My vision is to come alongside you as you discover those deep rooted desires, coach you through what may be stopping or slowing you down from achieving them, and cheer you on into a bright, productive and expressive future.
           </div>
 
-          <div>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;I am not a business coach, nor am I a counselor. What I am is a master of habit change. And often times, our own habits, developed over years of negative experience, discouragement, and disappointment keep us from seeing forward movement in our lives. The goal of Ziel Coaching is to identify those habits, discover which ones are helpful and which could be harmful, and then align those that serve you with your true identity, using encouragement, optimism and accountability as a guide.
-          </div>
-          <div>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;If you're tired of being stuck in a life you never pictured for yourself, I encourage you to find out what Ziel Coaching is all about. Click the link below to sign up for your 45-minute Personal Vision Breakthrough Session and see where Ziel can take you.
-          </div>
+            <div className="that-ziel">
+              "our own habits keep us from seeing forward movement in our lives"
+            </div>
 
-          <div className="now">Now is your time,</div>
+            <div>
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;I am not a business coach, nor am I a counselor. What I am is a master of habit change. And often times, our own habits, developed over years of negative experience, discouragement, and disappointment keep us from seeing forward movement in our lives. The goal of Ziel Coaching is to identify those habits, discover which ones are helpful and which could be harmful, and then align those that serve you with your true identity, using encouragement, optimism and accountability as a guide.
+            </div>
+            <div>
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;If you're tired of being stuck in a life you never pictured for yourself, I encourage you to find out what Ziel Coaching is all about. Click the link below to sign up for your 45-minute Personal Vision Breakthrough Session and see where Ziel can take you.
+            </div>
 
-          <div>Bridget Vogel Gregory</div>
-          Owner and Coach
+            <div className="now">Now is your time,</div>
+
+            <div className="bridget">Bridget Vogel Gregory</div>
+            <div className="owner">Owner and Coach</div>
           </div>
         </div>
       </div>
